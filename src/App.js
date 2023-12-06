4fa37fa8c8718b24719012a36c84adb13aaaddc0
sami <thesamii99@gmail.com>
2023-12-06 23:41:18 +0100

    some routing fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from "./logo.svg"
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" Component={Home}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" Component={Home}></Route>
+      </Routes>
     </>
   )
 }
